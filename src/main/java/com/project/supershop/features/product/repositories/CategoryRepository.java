3cc8531085d2802eea6d4553e46f5527171eb8b6
phi_ironman <nguyentruongphi15032003@gmail.com>
2024-07-11 16:43:31 +0700

    category

--- src/main/java/com/project/supershop/features/product/repositories/CategoryRepository.java
+++ src/main/java/com/project/supershop/features/product/repositories/CategoryRepository.java
@@ -2,12 +2,19 @@ package com.project.supershop.features.product.repositories;
 
 import com.project.supershop.features.product.domain.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
     Optional<Category> findByName(String name);
+    @Query("SELECT c FROM Category c LEFT JOIN FETCH c.categoryImages WHERE c.parentId = :parentId AND c.isChild = :isChild")
+    List<Category> findAllCategoriesWithImagesByParentIdAndIsChild(@Param("parentId") String parentId, @Param("isChild") Boolean isChild);
+    @Query("SELECT c FROM Category c LEFT JOIN FETCH c.categoryImages WHERE c.parentId = :parentId")
+    List<Category> findAllCategoriesWithImagesByParentId(@Param("parentId") String parentId);
 }
